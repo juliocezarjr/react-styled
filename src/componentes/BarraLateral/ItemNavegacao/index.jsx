fd665e1e45f3570fcfa1dc6258485533a5c61710
Julio Barreto <juliocezarjr@gmail.com>
2024-03-14 18:25:22 -0300

    Primeiro commit

---
+++ src/componentes/BarraLateral/ItemNavegacao/index.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const ItemListaEstilizado = styled.li`
+    font-size: 24px;
+    line-height: 29px;
+    margin-bottom: 30px;
+    cursor: pointer;
+    color: ${ props => props.$ativo ? '#7b78e5' : '#D9D9D9' };
+    display: flex;
+    align-items: center;
+    gap: 22px;
+`
+
+export default function ItemNavegacao({children, iconeAtivo,iconeInativo, ativo=false}) {
+    return(
+        <ItemListaEstilizado $ativo={ativo}>
+            <img src={ativo ? iconeAtivo : iconeInativo} alt=""/>
+            {children}
+        </ItemListaEstilizado>
+    )
+}
